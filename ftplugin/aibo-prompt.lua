b5ae0492f6fc9ba81f96bc2aee560caaeb5718ee
Alisue <lambdalisue@gmail.com>
2025-10-01 20:04:52 +0900

    fix: Fix <C-Enter> and <F5> mappings in insert mode of prompt buffer

--- ftplugin/aibo-prompt.lua
+++ ftplugin/aibo-prompt.lua
@@ -18,8 +18,10 @@ if not (cfg and cfg.no_default_mappings) then
   local opts = { buffer = bufnr, nowait = true, silent = true }
   vim.keymap.set({ "n", "i" }, "<C-g><C-o>", "<Plug>(aibo-send)", { buffer = bufnr, nowait = true })
   vim.keymap.set("n", "<CR>", "<Plug>(aibo-submit)", opts)
-  vim.keymap.set({ "n", "i" }, "<C-Enter>", "<Plug>(aibo-submit)<Cmd>q<CR>", opts)
-  vim.keymap.set({ "n", "i" }, "<F5>", "<Plug>(aibo-submit)<Cmd>q<CR>", opts)
+  vim.keymap.set("n", "<C-Enter>", "<Plug>(aibo-submit)<Cmd>q<CR>", opts)
+  vim.keymap.set("n", "<F5>", "<Plug>(aibo-submit)<Cmd>q<CR>", opts)
+  vim.keymap.set("i", "<C-Enter>", "<Esc><Plug>(aibo-submit)<Cmd>q<CR>", opts)
+  vim.keymap.set("i", "<F5>", "<Esc><Plug>(aibo-submit)<Cmd>q<CR>", opts)
   vim.keymap.set({ "n", "i" }, "<C-c>", "<Plug>(aibo-send)<Esc>", opts)
   vim.keymap.set({ "n", "i" }, "g<C-c>", "<Plug>(aibo-send)<C-c>", opts)
   vim.keymap.set({ "n", "i" }, "<C-l>", "<Plug>(aibo-send)<C-l>", opts)
