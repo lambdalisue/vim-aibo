7d6c3f074f8850128afdd6494fda5734c5833757
Alisue <lambdalisue@gmail.com>
2025-10-10 22:04:50 +0900

    feat: Add <C-Enter> and <F5> as submit shortcuts in aibo-console.lua

--- ftplugin/aibo-console.lua
+++ ftplugin/aibo-console.lua
@@ -21,6 +21,8 @@ if not (cfg and cfg.no_default_mappings) then
   local opts = { buffer = bufnr, nowait = true, silent = true }
   vim.keymap.set("n", "<C-g><C-o>", "<Plug>(aibo-send)", { buffer = bufnr, nowait = true })
   vim.keymap.set("n", "<CR>", "<Plug>(aibo-submit)", opts)
+  vim.keymap.set("n", "<C-Enter>", "<Plug>(aibo-submit)", opts)
+  vim.keymap.set("n", "<F5>", "<Plug>(aibo-submit)", opts)
   vim.keymap.set("n", "<C-c>", "<Plug>(aibo-send)<Esc>", opts)
   vim.keymap.set("n", "g<C-c>", "<Plug>(aibo-send)<C-c>", opts)
   vim.keymap.set("n", "<C-l>", "<Plug>(aibo-send)<C-l>", opts)
